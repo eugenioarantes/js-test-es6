89984706da252626150e003dba52472ff4738da5
Eugenio <eugenilopesarantesjunior@gmail.com>
2022-03-08 15:29:58 -0300

    correction of tests es6

--- src/vqv.js
+++ src/vqv.js
@@ -19,8 +19,6 @@
 const vqv = (name, age) => {
   if (!((name) || (age))) return undefined;
 
-  const lineBreak = `\n`;
-
   return `Oi, meu nome é ${name}!
 Tenho ${age} anos,
 trabalho na Trybe e mando muito em programação!
